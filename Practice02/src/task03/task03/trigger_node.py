aae6a59965b4d47dda4b72273fa6f91c188fcd03
Sofia Petrenko <sofia.petrenko@jetbrains.com>
2026-02-16 17:19:38 +0200

    fix solution

--- Practice02/src/task03/task03/trigger_node.py
+++ Practice02/src/task03/task03/trigger_node.py
@@ -9,37 +9,34 @@ class TriggerNode(Node):
         self.declare_parameter('service_name', '/trigger_service')
         self.declare_parameter('default_string', 'No service available')
 
-        service_name = self.get_parameter('service_name').get_parameter_value().string_value
-        self.default_string = self.get_parameter('default_string').get_parameter_value().string_value
+        self.stored_string = self.get_parameter(
+            'default_string'
+        ).get_parameter_value().string_value
 
-        self.stored_string = self.default_string
+        self._fetch_trigger_response()
 
-        self.client = self.create_client(Trigger, '/spgc/trigger')
-        self.call_trigger_service()
+        srv_name = self.get_parameter(
+            'service_name'
+        ).get_parameter_value().string_value
+        self.create_service(Trigger, srv_name, self._handle_request)
+        self.get_logger().info(f'Providing service on "{srv_name}"')
 
-        self.srv = self.create_service(Trigger, service_name, self.service_callback)
-        self.get_logger().info(
-            f'Service "{service_name}" ready, stored string: "{self.stored_string}"'
-        )
+    def _fetch_trigger_response(self):
+        cli = self.create_client(Trigger, '/spgc/trigger')
+        if not cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().warn('Could not reach /spgc/trigger')
+            return
 
-    def call_trigger_service(self):
-        if self.client.wait_for_service(timeout_sec=5.0):
-            request = Trigger.Request()
-            future = self.client.call_async(request)
-            rclpy.spin_until_future_complete(self, future, timeout_sec=5.0)
-            if future.result() is not None:
-                self.stored_string = future.result().message
-                self.get_logger().info(
-                    f'Received from /spgc/trigger: "{self.stored_string}"'
-                )
-            else:
-                self.get_logger().warn('Service call to /spgc/trigger failed')
+        future = cli.call_async(Trigger.Request())
+        rclpy.spin_until_future_complete(self, future)
+        result = future.result()
+        if result is not None:
+            self.stored_string = result.message
+            self.get_logger().info(f'Trigger returned: "{self.stored_string}"')
         else:
-            self.get_logger().warn(
-                'Service /spgc/trigger not available, using default string'
-            )
+            self.get_logger().warn('Trigger call returned no result')
 
-    def service_callback(self, request, response):
+    def _handle_request(self, request, response):
         response.success = True
         response.message = self.stored_string
         return response
