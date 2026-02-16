2ada3febb78b91512b9b565b832fa4e5b13c128b
Sofia Petrenko <sofia.petrenko@jetbrains.com>
2026-02-16 16:57:04 +0200

    fix solution

--- Practice02/src/task03/setup.py
+++ Practice02/src/task03/setup.py
@@ -9,7 +9,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/task3.launch']),
+        ('share/' + package_name + '/launch', ['launch/task03.launch']),
         ('share/' + package_name + '/config', ['config/task03.yaml']),
     ],
     install_requires=['setuptools'],
